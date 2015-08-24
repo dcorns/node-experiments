f90440060d17c7e6b2166eb331a8fc0753602429
Dale Corns <drcorns@hotmail.com>
2015-08-23 19:27:55 -0700

    finish custom events and readline example

--- nodeEvents.js
+++ nodeEvents.js
@@ -2,108 +2,50 @@
  * nodeEvents
  * Created by dcorns on 8/20/15.
  * This code demonstrates adding custom events and using readline (note: readline was unstable at the time of this crafting)
+ * It is a trivial survey program that takes in an array of question and then collects an array of answers.
+ * The survey can only be run once per instantiation.
  */
 'use strict';
+//require dependencies
 var EventEmitter = require('events').EventEmitter,
-  readline = require('readline');
-var Counter = function(num){
-  this.increment = function(){
-    num++;
-    this.emit('incremented', num);
-  }
-};
-Counter.prototype = new EventEmitter();
-var counter = new Counter(1);
-var cb = function(count){
-  console.log(count);
-};
+  readline = require('readline'),
 
-counter.addListener('incremented', cb);
-Counter.prototype.hello = function(str){
-  console.log('Hello' + str);
-};
-
-
-var r = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-var Survey = function(questions){
+Survey = function(questions){
   this.questions = questions || [];
   this.answers = [];
+  this.idx = 0;
+  //the readline interface is created when instantiated so when it is closed, it will no longer work
   this.r = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: false
   });
-  this.ask = function(q){
+  //a recursive function that handles processing of all the questions/answers then sends answers to original caller
+  this.ask = function(cb){
     var ctx = this;
-    this.r.question(q, function(data){
-      ctx.answers.push(data);
-      ctx.r.close();
-      ctx.emit('answered', ctx.answers);
-    });
+    if(this.questions.length > this.idx){
+      this.r.question(this.questions[this.idx++] + ' ? ', function(data){
+        ctx.answers.push(data);
+        ctx.emit('answered', data); //the custom emitter notify when a question has been answered
+        ctx.ask(cb); //the recursive call
+      });
+    }
+    else{
+      this.r.close(); //Once close the survey will not be able to run again
+      cb(this.answers);
+    }
+
   }
 };
 
-Survey.prototype = new EventEmitter();
-
-//Survey.prototype.ask = function(q){
-//  var ctx = this;
-//  this.r.question(q, function(data){
-//    ctx.answers.push(data);
-//    ctx.r.close();
-//    this.emit('answered', ctx.answers);
-//  });
-//};
-
-Survey.prototype.run = function(){
-  this.ask(this.questions[0] + '? ');
-  console.log(this.answers);
-};
-
+Survey.prototype = new EventEmitter(); //This is where we get our custom events
 
 var survey = new Survey(['Name', 'Gender', 'Date of Birth']);
-survey.on('answered', function(data){
-  console.dir(data + ' answered');
-  survey.ask();
+
+survey.on('answered', function(data){ //nothing important here except it demonstrates the use of the custom emitter.
+  console.log(data);
 });
-survey.run();
 
-
-//survey.questions.forEach(function(q){
-//  console.log(q);
-//});
-//var a = [];
-//function ask(cb){
-//  r.question('What is your Name? ', function(data){
-//    a.push(data);
-//    r.question('dob? ', function(data){
-//      a.push(data);
-//      cb();
-//    });
-//  });
-//
-//}
-
-
-
-//ask(function(){
-//  console.log('Hello ' + a);
-//  r.close();
-//});
-
-
-
-
-
-
-
-
-
-//counter.increment();
-//counter.increment();
-//counter.increment();
-//counter.hello('Dale');
-//counter.increment();
+survey.ask(function(data){
+  console.log(data);
+});
